a82021f4ec67c22a8ebe1912dae1785695bfa35c
Daniel <dwgrossberg@gmail.com>
2022-10-06 13:56:44 -0400

    Update index and home styles

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,10 @@ import "./home.scss";
 const home = () => {
   return (
     <div className="home">
-      <h2>anything you want store</h2>
+      <div className="title">
+        <h2>the anything you want store</h2>
+        <p>voted the #1 online retailer of 2023</p>
+      </div>
     </div>
   );
 };
